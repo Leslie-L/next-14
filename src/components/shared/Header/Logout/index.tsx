d2719e5d040b51fdae283b7e2f5ad3225ce03b18
Leslie-L <leslie.centeno@galileo.edu>
2024-02-07 20:51:39 -0600

    arreglando el log out

--- src/components/shared/Header/Logout/index.tsx
+++ src/components/shared/Header/Logout/index.tsx
@@ -1,8 +1,13 @@
 "use client"
-import logOut from "@/firebase/logout";
+import { handleLogOut } from '@/actions'
 import styles from '../Header.module.css'
 export default function Logout() {
+    const handleAction = async (event) => {
+        
+        await handleLogOut();
+      }
+    
     return(
-        <button onClick={logOut} className={styles.item}>Log out</button>
+        <button onClick={handleAction} className={styles.item}>Log out</button>
     )
 }
