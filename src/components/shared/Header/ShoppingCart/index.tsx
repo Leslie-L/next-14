e47824a736bb902a82d1fcc1e44486d3ff3cbca7
Leslie-L <leslie.centeno@galileo.edu>
2024-02-08 18:04:17 -0600

    ventana de detalle del carrito

--- src/components/shared/Header/ShoppingCart/index.tsx
+++ src/components/shared/Header/ShoppingCart/index.tsx
@@ -2,17 +2,35 @@
 import { FaShoppingCart } from "react-icons/fa";
 import styles from './ShoppingCart.module.css'
 import { useShoppingCart } from "@/provider/useShoppingCart";
+import { useState } from "react";
 
 export const ShoppingCart = () => {
   const { cart } = useShoppingCart();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(!isOpen);
   console.log(cart)
   const total = cart.reduce((prev,current)=>current.quantity+prev,0)
   return (
-    <button className={styles.ShoppingCart}>
+    <button className={styles.ShoppingCart} onClick={handleOpen}>
       <span className={styles.ShoppingCart__counter}>
         {total}
       </span>
       <FaShoppingCart />
+      {isOpen && (
+        <div className={styles.ShoppingCart__items}>
+          {
+            cart.map(item => (
+              <div key={item?.id} className={styles.ShoppingCart__items_item}>
+                <p >{item?.title}</p>
+                <p><b>Cantidad:</b> {item.quantity}</p>
+              </div>
+            ))
+          }
+          <button className={styles.ShoppingCart__buyButton}>
+            Buy
+          </button>
+        </div>
+      )}
     </button>
   )
 }
