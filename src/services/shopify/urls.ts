53b3236531b4628174d31b864a63297a17053b21
Leslie-L <leslie.centeno@galileo.edu>
2024-02-20 21:02:09 -0600

    Adding local variable

--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -2,11 +2,11 @@ import { env } from "@/config/env";
 
 export const shopifyUrls = {
     products:{
-        'all':`http://localhost:3000/api/products`,
+        'all':`${process.env.NEXT_PUBLIC_API_LOCAL}/api/products`,
         'mainProducts': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/465664934182/products.json`,
     },
     collections: {
-        'all': `http://localhost:3000/api/collections`,
+        'all': `${process.env.NEXT_PUBLIC_API_LOCAL}/api/collections`,
         'products': (id: string) => `https://${env.SHOPIFY_HOSTNAME}/admin/api/2024-01/collections/${id}/products.json`
     }
 }
